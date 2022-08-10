04ce471749d99927068161b58e4ce1e77bd76228
AmitBBhatti <amitbhatti241289@gmail.com>
2022-08-10 15:38:07 +0530

    firstcommit

--- library/src/main/java/com/example/library/BlinkEffect.kt
+++ library/src/main/java/com/example/library/BlinkEffect.kt
@@ -22,4 +22,19 @@ object BlinkEffect {
         animator.repeatCount = Animation.INFINITE
         animator.start()
     }
+    fun blink2(view: View) {
+        // adding the color to be shown
+        val animator: ObjectAnimator = ObjectAnimator.ofInt(
+            view, "backgroundColor", Color.YELLOW,
+            Color.RED, Color.GREEN
+        )
+        // duration of one color
+        animator.duration = 500;
+        animator.setEvaluator(ArgbEvaluator())
+        // color will be shown in reverse manner
+        animator.repeatCount = Animation.REVERSE
+        // Repeat up to infinite time
+        animator.repeatCount = Animation.INFINITE
+        animator.start()
+    }
 }
